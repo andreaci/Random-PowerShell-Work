ebdb6cc3cf7342271ffa10241d3a4637f7a8dde4
Andrea Corti <ascorti@softeam.it>
2022-10-04 14:27:48 +0200

    Added Get-MFAStatus and BackupBitLockerKey

---
+++ Windows/backup_bitlocker_key.ps1
@@ -0,0 +1,12 @@
+﻿$drives = Get-PSDrive -PSProvider FileSystem | select @{name="Name"; expression={ $_.Name+":"}}
+
+foreach ($drive in $drives){
+ 
+    #Enable-Bitlocker -MountPoint $drive.Name -EncryptionMehod -UsedSpaceOnly -PasswordProtector -SkipHardwareTest
+    
+    $BitVolume = Get-BitLockerVolume -MountPoint $drive.Name
+    $RecoveryKey = $BitVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' }
+
+    Backup-BitLockerKeyProtector -MountPoint $drive.Name -KeyProtectorId $RecoveryKey.KeyProtectorID
+    BackupToAAD-BitLockerKeyProtector -MountPoint $drive.Name -KeyProtectorId $RecoveryKey.KeyProtectorID
+}
